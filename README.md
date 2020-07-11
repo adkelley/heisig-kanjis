b37a6224ff7d7064aceb6090a92f4c6f910d69d9
Alex Kelley <adkelley@users.noreply.github.com>
2020-07-11 15:52:37 -0700

    added <option> to retrieve RTK keywords or number

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # heisig-kanjis
-The file heisig-kanjis.csv was imported into Google Sheets.  In turn, using the Google Sheets API (V4) 'index.js' takes
-a kanji or kanji compound and returns the corresponding RTK keywords to the clipboard
+The file heisig-kanjis.csv was cloned from 'sdcr/heisig-kanjis' and imported into Google Sheets.  In turn, using the Google Sheets API (V4), `node index.js <option> <compound>` takes
+a kanji or kanji compound and returns the corresponding RTK keywords or numbers to the clipboard as a string.
+
+## Setup
+1. Import the file heisig-kanjis.csv into Google Sheets
+2. Create your api endpoint and download your credentials to `api-keys.json`.  See this [tutorial](https://developers.google.com/sheets/api/quickstart/nodejs) for further details.
 
 ## Usage
-    node index.js 新聞
-returns 'new, hear'
+```bash
+$ node index.js <option> <compound>
+```
+where:
+* <option>
+
+   - **kw** will copy the corresponding Heisig keywords for <compound> to the clipboard (Mac)
+   - **ix** will copy the RTK (6th edition) indices for <compound> to the clipboard (Mac)
+* <compound> is a kanji character or compound.  For example, **新聞** is the kanji <compound> for newspaper
 
 ## CSV file format
-The file heisig-kanjis.csv contains the kanji's featured in James Heisig's "Remembering the Kanji" 1 & 3.
+The file heisig-kanjis.csv contains the kanji's featured in James Heisig's "Remembering the Kanji" 1 & 3. Import this file into Google Sheets
 Each line of the CSV file is structured as follows:
 
 Field name | description
--- index.js
+++ index.js
@@ -19,33 +19,36 @@ client.authorize(function(err, tokens) {
 });
 
 
-// Given a kanji, return the keyword from RTK 6th Edition
-function findKeyword(kanji, kanjis, keywords) {
+// Given a kanji, return the keyword or index from RTK 6th Edition
+function findResult(kanji, kanjis, results) {
+	let result = '';
+
 	for (let i=0; i < kanjis.length; i++) {
 		if (kanjis[i] === kanji) {
-			return keywords[i];
+			result = results[i];
+			break;
 		}
 	}
 
-	return '';
+	return result;
 	
 }
 
 /*
 ** Given a kanji or kanji compound, return a string containing
-** the keyword or all the keywords making up that compound, respectively
+** the result or all the results making up that kanji or compound,
+** respectively. results are a keyword(s) or index(es)
 */
-function getKeywords(compound, kanjis, keywords) {
+function getResults(compound, kanjis, results) {
 	const compoundKanjis = compound.split('');
 
 	let currentValue = '';
 	compoundKanjis.forEach(kanji => {
-		currentValue += findKeyword(kanji, kanjis, keywords) + ', ';
+		currentValue += findResult(kanji, kanjis, results) + ', ';
 	});
 	
 	return currentValue.slice(0, -2);
 }
-
 
 function pbcopy(data) {
 	const proc = require('child_process').spawn('pbcopy');
@@ -55,21 +58,32 @@ function pbcopy(data) {
 
 async function gsrun(cl) {
 	const gsapi = google.sheets({version: 'v4', auth: cl});
-	// Column A: kanjis, Column E: keywords (6th edition)
+	// Column A: kanjis, Column C: Index Column E: keywords (6th edition)
 	const opt = {
 		spreadsheetId: '1woYl-4S7c37pyHQKFAplinGTc9TxQZN-gL8O61RfbSk',
-		ranges: ['Heisig!A2:A3001', 'Heisig!E2:E3001'],
+		ranges: ['Heisig!A2:A3001', 'Heisig!C2:C3001', 'Heisig!E2:E3001'],
 		majorDimension: 'COLUMNS'
 	};
-
 	const data = await gsapi.spreadsheets.values.batchGet(opt);
-	const compound = process.argv[2];
 	const kanjis = data.data.valueRanges[0].values[0];
-	const keywords = data.data.valueRanges[1].values[0];
+	const indices = data.data.valueRanges[1].values[0];
+	const keywords = data.data.valueRanges[2].values[0];
 
-	const text = getKeywords(compound, kanjis, keywords);
+	const option  = process.argv[2];
+	const compound = process.argv[3];
 
-	pbcopy(text);
-
-	
+	switch (option) {
+		case 'kw': {
+			pbcopy(getResults(compound, kanjis, keywords));
+			break;
+		}
+		case 'ix': {
+			pbcopy(getResults(compound, kanjis, indices));
+			break;
+		}
+		default: {
+			console.log('Error: invalid option');
+			pbcopy('');
+		}
+	}
 }
